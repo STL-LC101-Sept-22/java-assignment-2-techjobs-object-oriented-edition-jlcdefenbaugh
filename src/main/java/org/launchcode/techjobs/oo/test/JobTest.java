c1e4e3647f369275206d80ac66d55c1213b0bc23
Jami <jamidefenbaugh@gmail.com>
2023-01-30 15:59:15 -0600

    passed testJobConstructorSetsAllFields

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -3,8 +3,9 @@ package org.launchcode.techjobs.oo.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.launchcode.techjobs.oo.Job;
-import static org.junit.Assert.assertNotEquals;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
@@ -17,6 +18,21 @@ public class JobTest {
         Job job1 = new Job();
         Job job2 = new Job();
         assertNotEquals(job1.getId(), job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(job3 instanceof Job);
+        assertEquals("Product tester", job3.getName());
+        assertTrue(job3.getEmployer() instanceof Employer);
+        assertEquals("ACME", job3.getEmployer().toString());
+        assertTrue(job3.getLocation() instanceof Location);
+        assertEquals("Desert", job3.getLocation().toString());
+        assertTrue(job3.getPositionType() instanceof PositionType);
+        assertEquals("Quality control", job3.getPositionType().toString());
+        assertTrue(job3.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals("Persistence", job3.getCoreCompetency().toString());
     }
 
 }
